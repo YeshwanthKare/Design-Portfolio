11bb123763bcaaec5470a79deb9d07400fa2cba8
YeshwanthKare <yesh.kare@gmail.com>
2021-05-14 22:03:31 +0530

    portfolio

--- script.js
+++ script.js
@@ -3,8 +3,8 @@
 window.onscroll = () => {scrollFunction()}
 
 const scrollFunction = () => {
-    if(document.body.scrollTop > 1 || document.documentElement.scrollTop > 1) {
-        document.querySelector(".header-container").style.padding = "14px 100px 14px 100px";
+    if(document.body.scrollTop > 60 || document.documentElement.scrollTop > 60) {
+        document.querySelector(".header-container").style.padding = "8px 100px 8px 100px";
     }else {
     document.querySelector(".header-container").style.padding = "44px 100px 44px 100px";
 
