584458740e9d9cdeb7c732383ba561b314a5a26c
YeshwanthKare <yesh.kare@gmail.com>
2021-06-26 00:57:39 +0530

    CHNG: chnage the images ext

--- scripts/developer-projects.js
+++ scripts/developer-projects.js
@@ -2,19 +2,19 @@ let developerProjects = [
     {
 	projectLink: "https://yesh-react-amazon-cart.netlify.app",
 	buttonText: "React Amazon Cart",
-	imageSrc: "./assets/amazon-cart.JPG"
+	imageSrc: "/assets/amazon-cart.JPG"
     },
     {
 	
 	projectLink : "https://react-todo-app-ea10d.web.app",
 	buttonText : "React Todo App",
-	imageSrc : "./assets/to-do.PNG"
+	imageSrc : "/assets/to-do.PNG"
 	    
     },
     {
 	projectLink: "https://image-gram-test.herokuapp.com/",
 	buttonText: "ImageGram",
-	imageSrc: "./assets/imagegram.JPG"
+	imageSrc: "/assets/imagegram.JPG"
     },
     {
 	projectLink: "https://netflix-clone-c0ee4.web.app",
@@ -24,28 +24,28 @@ let developerProjects = [
     {
 	projectLink: "https://my-blog-app-2ffbe.web.app",
 	buttonText: "My Blog App",
-	imageSrc: "./assets/blog.JPG"
+	imageSrc: "/assets/blog.JPG"
     },
     {
 	
 	projectLink: "https://yeshwanthkare.github.io/Beat-BoxJS/",
 	buttonText: "Beat Box JS",
-	imageSrc: "./assets/beat-box.JPG"
+	imageSrc: "/assets/beat-box.JPG"
 	    
     },
     {
 	projectLink: "https://yeshwanthkare.github.io/Weather-App/",
 	buttonText: "Weather App",
-	imageSrc: "./assets/weatherapp.jpg"
+	imageSrc: "/assets/weatherapp.jpg"
     },
     {
 	projectLink: "https://yeshwanthkare.github.io/Text-Editor/",
 	buttonText: "Text Editor",
-	imageSrc: "./assets/text-editor.JPG"    
+	imageSrc: "/assets/text-editor.JPG"    
     },
     {
 	projectLink: " https://yeshwanthkare.github.io/Guess-the-Number-Game/",
 	buttonText: "Guess the Number",
-	imageSrc: "./assets/guess.JPG"    
+	imageSrc: "/assets/guess.JPG"    
     }
 ]
