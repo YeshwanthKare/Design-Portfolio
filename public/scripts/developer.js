4ae2e2b708e63d19bbaa45623b965d6b0fd02dc4
YeshwanthKare <yesh.kare@gmail.com>
2021-06-27 02:21:20 +0530

    CHNG: changed the JS in develop

--- public/scripts/developer.js
+++ public/scripts/developer.js
@@ -3,6 +3,15 @@ window.onload = () => {
 }
 window.onscroll = () => {scrollFunction(".work-header")}
 
+const scrollFunction = (header) => {
+    if(document.body.scrollTop > 1 || document.documentElement.scrollTop > 1) {
+        document.querySelector(header).style.padding = "8px 100px 8px 100px";
+    }else {
+    document.querySelector(header).style.padding = "44px 100px 44px 100px";
+
+  }
+}
+
 const developerProject = () => {
   let projectsContainer = document.querySelector(".develop-projects-container")
 
