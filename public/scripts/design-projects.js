c55cfc4b3edaaedd4539084fea3b07277ee6af3b
YeshwanthKare <yesh.kare@gmail.com>
2021-07-03 23:43:06 +0530

    ADD: added houston movies to project list

--- public/scripts/design-projects.js
+++ public/scripts/design-projects.js
@@ -3,5 +3,10 @@ const designProjects = [
 	projectLink : "/html/coffeehouse.html",
 	buttonText : "Coffee House",
 	imageSrc : "/assets/coffeeHouse.jpg"    
+    },
+    {
+	    projectLink: "/html/houston-movies.html",
+	    buttonText: "Houston Movies",
+	    imageSrc: "/assets/hm-logo.png"
     }
 ]
