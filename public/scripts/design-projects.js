9ef11c9ec2f5bf91e8992def94043969ee330987
YeshwanthKare <yesh.kare@gmail.com>
2021-07-27 01:13:52 +0530

    ADD: completed the design third portfolio project

--- public/scripts/design-projects.js
+++ public/scripts/design-projects.js
@@ -1,12 +1,17 @@
-const designProjects = [ 
-    {
-	projectLink : "/html/coffeehouse.html",
-	buttonText : "Coffee House",
-	imageSrc : "/assets/coffeeHouse.jpg"    
-    },
-    {
-	    projectLink: "/html/houston-movies.html",
-	    buttonText: "Houston Movies",
-	    imageSrc: "/assets/hm-logo.PNG"
-    }
-]
+const designProjects = [
+  {
+    projectLink: "/html/startup.html",
+    buttonText: "Let's StartUp",
+    imageSrc: "/assets/start-logo.PNG",
+  },
+  {
+    projectLink: "/html/houston-movies.html",
+    buttonText: "Houston Movies",
+    imageSrc: "/assets/hm-logo.PNG",
+  },
+  {
+    projectLink: "/html/coffeehouse.html",
+    buttonText: "Coffee House",
+    imageSrc: "/assets/coffeeHouse.jpg",
+  },
+];
