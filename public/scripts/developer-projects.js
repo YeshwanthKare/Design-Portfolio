81996b9f5abcadba94f1c93267e91e77f90cae44
YeshwanthKare <yesh.kare@gmail.com>
2021-12-10 12:55:51 +0100

    ADD: updating with new Projects

--- public/scripts/developer-projects.js
+++ public/scripts/developer-projects.js
@@ -1,51 +1,79 @@
 let developerProjects = [
-    {
-	projectLink: "https://yesh-react-amazon-cart.netlify.app",
-	buttonText: "React Amazon Cart",
-	imageSrc: "/assets/amazon-cart.png"
-    },
-    {
-	
-	projectLink : "https://react-todo-app-ea10d.web.app",
-	buttonText : "React Todo App",
-	imageSrc : "/assets/to-do.PNG"
-	    
-    },
-    {
-	projectLink: "https://image-gram-test.herokuapp.com/",
-	buttonText: "ImageGram",
-	imageSrc: "/assets/imagegram.png"
-    },
-    {
-	projectLink: "https://yesh-netflix.netlify.app",
-	buttonText: "Netflix Clone",
-	imageSrc: "/assets/netflix.JPG"
-    },
-    {
-	projectLink: "https://my-blog-app-2ffbe.web.app",
-	buttonText: "My Blog App",
-	imageSrc: "/assets/blog.JPG"
-    },
-    {
-	
-	projectLink: "https://yeshwanthkare.github.io/Beat-BoxJS/",
-	buttonText: "Beat Box JS",
-	imageSrc: "/assets/beat-box.JPG"
-	    
-    },
-    {
-	projectLink: "https://yeshwanthkare.github.io/Weather-App/",
-	buttonText: "Weather App",
-	imageSrc: "/assets/weatherapp.jpg"
-    },
-    {
-	projectLink: "https://yeshwanthkare.github.io/Text-Editor/",
-	buttonText: "Text Editor",
-	imageSrc: "/assets/text-editor.png"    
-    },
-    {
-	projectLink: " https://yeshwanthkare.github.io/Guess-the-Number-Game/",
-	buttonText: "Guess the Number",
-	imageSrc: "/assets/guess.png"    
-    }
-]
+  {
+    projectLink: "https://react-netflix-clone-ed2de.web.app/",
+    buttonText: "React Netflix Clone",
+    imageSrc: "/assets/netflix.JPG",
+  },
+  {
+    projectLink: "https://yesh-tictactoe.surge.sh/",
+    buttonText: "React Tic Tac Toe",
+    imageSrc: "/assets/react-tic-tac-toe.JPG",
+  },
+  {
+    projectLink: "http://yeshwanthkare.github.io/react-box-office/",
+    buttonText: "React Box Office",
+    imageSrc: "/assets/box-office.JPG",
+  },
+  {
+    projectLink: "https://expenses-calc.netlify.app/",
+    buttonText: "React Expenses Calculator",
+    imageSrc: "/assets/react-expenses.JPG",
+  },
+  {
+    projectLink: "https://yesh-sunnyside-landing-page.netlify.app/",
+    buttonText: "Sunny Side Landing Page",
+    imageSrc: "/assets/sunny-side.JPG",
+  },
+  {
+    projectLink: "https://inkoop-landing-page.netlify.app/",
+    buttonText: "Inkoop Landing Page",
+    imageSrc: "/assets/Inkoop.JPG",
+  },
+
+  {
+    projectLink: "https://yeshwanthkare.github.io/TributePage/",
+    buttonText: "Tribute Page",
+    imageSrc: "/assets/tribute.JPG",
+  },
+  {
+    projectLink: "https://yesh-react-amazon-cart.netlify.app",
+    buttonText: "React Amazon Cart",
+    imageSrc: "/assets/amazon-cart.png",
+  },
+  {
+    projectLink: "https://react-todo-app-ea10d.web.app",
+    buttonText: "React Todo App",
+    imageSrc: "/assets/to-do.PNG",
+  },
+  {
+    projectLink: "https://image-gram-test.herokuapp.com/",
+    buttonText: "ImageGram",
+    imageSrc: "/assets/imagegram.png",
+  },
+
+  {
+    projectLink: "https://my-blog-app-2ffbe.web.app",
+    buttonText: "My Blog App",
+    imageSrc: "/assets/blog.JPG",
+  },
+  {
+    projectLink: "https://yeshwanthkare.github.io/Beat-BoxJS/",
+    buttonText: "Beat Box JS",
+    imageSrc: "/assets/beat-box.JPG",
+  },
+  {
+    projectLink: "https://yeshwanthkare.github.io/Weather-App/",
+    buttonText: "Weather App",
+    imageSrc: "/assets/weatherapp.jpg",
+  },
+  {
+    projectLink: "https://yeshwanthkare.github.io/Text-Editor/",
+    buttonText: "Text Editor",
+    imageSrc: "/assets/text-editor.png",
+  },
+  {
+    projectLink: " https://yeshwanthkare.github.io/Guess-the-Number-Game/",
+    buttonText: "Guess the Number",
+    imageSrc: "/assets/guess.png",
+  },
+];
